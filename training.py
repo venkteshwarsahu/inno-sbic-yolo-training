b8a8e63e91d68b31c318e19f82efb313bb7080ca
Venkteshwar Sahu <venktesh@LMP-Venkteshwar.local>
2026-02-09 18:17:33 +0530

    minor changes

--- training.py
+++ training.py
@@ -4,16 +4,20 @@ logging.basicConfig(level=logging.INFO)
 
 logging.info("Training model...")
 model = YOLO("yolo26s.pt")
-# train_results = model.train(
-#     data="dataset/aadhaar.yaml",
-#     epochs=100,
-#     imgsz=640,
-#     device="cpu",
-# )
+train_results = model.train(
+    data="aadhaar-classification-dataset/aadhaar.yaml",
+    epochs=200,
+    imgsz=640,
+    device="0",
+)
 
-# metrics = model.val()
-# logging.info(metrics)
+metrics = model.val()
+logging.info("Validation metrics:")
+logging.info(metrics)
 results = model("image402.jpg")
+logging.info("Results:")
 logging.info(results)
+
+
 # path = model.export(format="onnx")
 # logging.info(f"Model exported to {path}")
